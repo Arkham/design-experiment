06e3487257bc0f53a60dc34330bb3ae7dc424fe2
Ju Liu <ju.liu@welaika.com>
2014-01-22 14:18:51 +0100

    improved searching algorithm

--- app/services/search_member_connections.rb
+++ app/services/search_member_connections.rb
@@ -8,36 +8,46 @@ class SearchMemberConnections
   end
 
   def search_topic(topic)
-    ActiveRecord::Base.connection.execute(sql_for(topic)).map do |result|
-      SearchResult.new({
-        path: result["path"].scan(/\d+/).map{|id| Member.find(id) },
+    ActiveRecord::Base.connection.execute(sql_for(topic)).each_with_object({}) do |item, result|
+      item = OpenStruct.new(item)
+
+      result[item.match_id] ||= SearchResult.new({
+        path: item.path.scan(/\d+/).map{|id| Member.find(id) },
         match: topic
       })
-    end
+    end.values
   end
 
   class SearchResult < OpenStruct; end
 
   private
 
+  def connection_minimum_depth
+    2
+  end
+
   def sql_for(topic)
+    like_topic = "%#{topic}%"
+
     tree_sql = <<-SQL
-      WITH RECURSIVE friendship_tree(id, name, path) AS (
-        SELECT id, name, ARRAY[id]
+      WITH RECURSIVE friendship_tree(id, name, match_id, matched_h1, path) AS (
+        SELECT id, name, id, CASE WHEN members.h1 LIKE ? then 1 else 0 end, ARRAY[id]
         FROM members
-        WHERE members.h1 LIKE ?
+        WHERE members.h1 LIKE ? OR members.h2 LIKE ? OR members.h3 LIKE ?
       UNION ALL
-        SELECT members.id, members.name, members.id || path
-        FROM friendship_tree
-        JOIN friendships ON friendships.friend_id = friendship_tree.id
+        SELECT members.id, members.name, ft.match_id, ft.matched_h1, members.id || path
+        FROM friendship_tree AS ft
+        JOIN friendships ON friendships.friend_id = ft.id
         JOIN members ON members.id = friendships.member_id
         WHERE NOT members.id = ANY(path)
       )
       SELECT * FROM friendship_tree
-      WHERE id = ? AND array_length(path, 1) > 2
-      ORDER BY array_length(path, 1);
+      WHERE id = ? AND array_length(path, 1) > ?
+      ORDER BY matched_h1 desc, array_length(path, 1);
     SQL
 
-    ActiveRecord::Base.send(:sanitize_sql_array, [tree_sql, "%#{topic}%", member.id])
+    ActiveRecord::Base.send(:sanitize_sql_array, [tree_sql,
+                                                  like_topic, like_topic, like_topic, like_topic,
+                                                  member.id, connection_minimum_depth])
   end
 end
--- spec/services/search_member_connections_spec.rb
+++ spec/services/search_member_connections_spec.rb
@@ -19,5 +19,47 @@ describe SearchMemberConnections do
       expect(first.path).to eq([member, friend, expert])
       expect(first.match).to eq("Ruby Love")
     end
+
+    context "multiple headers" do
+      let(:other) { create(:member, name: "Other", h3: "Metaprogramming") }
+      before do
+        Friendship.create(member: expert, friend: other)
+      end
+
+      it "supports multiple header matching" do
+        first = service.search_topic("Metaprogramming").first
+        expect(first.path).to eq([member, friend, expert, other])
+      end
+    end
+
+    context "multiple matches" do
+      let(:other) { create(:member, name: "Other", h1: "Metaprogramming") }
+      before do
+        expert.update_attributes(h2: "Metaprogramming")
+        Friendship.create(member: expert, friend: other)
+      end
+
+      it "prefers h1 matches over h2 matches" do
+        first, second = service.search_topic("Metaprogramming")
+        expect(first.path).to eq([member, friend, expert, other])
+        expect(second.path).to eq([member, friend, expert])
+      end
+    end
+
+    context "multiple paths" do
+      let(:other) { create(:member, name: "Other") }
+      before do
+        Friendship.create(member: friend, friend: other)
+        Friendship.create(member: other, friend: expert)
+      end
+
+      it "returns only the shortest path to an expert" do
+        expect(service.search_topic("Ruby Love").count).to eq(1)
+        expect(service.search_topic("Ruby Love").first.path).to eq([
+          member, friend, expert
+        ])
+      end
+    end
+
   end
 end
